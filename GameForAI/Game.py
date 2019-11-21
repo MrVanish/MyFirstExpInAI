0df4c5052d1ca06f12e930dbb8e18cebcecae230
Альберт Дударевич <albertdudarevic@gmail.com>
2019-11-21 19:25:50 +0300

    application framework added

--- GameForAI/Game.py
+++ GameForAI/Game.py
@@ -6,12 +6,20 @@ class GameScreen:
 		for i in pygame.event.get():
 			if i.type == pygame.QUIT:
 				exit()
+		###################################
 		
+		###################################
+		pygame.display.update()
 
 	def __init__(self):
 		pygame.init()
 		self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+		self.clock = pygame.time.Clock()
+		pygame.display.update()
 		while True:
+			self.clock.tick(120)
 			self.update()
+			
+		
 
 GameScreen()
